c2e5618f457dcd9acd33950b27cf5fff785f940f
VrijVlinder <adalib@mac.com>
2014-02-21 22:22:58 -0600

    New

---
+++ about.php
@@ -0,0 +1,21 @@
+<?php if (!defined('APPLICATION')) exit();
+/*
+Copyright Creative Commons 2012 VrijVlinder
+This file is part of Garden.
+Garden is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Garden.  If not, see <http://www.gnu.org/licenses/>.
+Contact VrijVlinder at contact [at] vrijvlinder [dot] com
+*/
+
+/**
+ * An associative array of information about this application.
+ */
+$ThemeInfo['YellowMobile'] = array(
+   'Name' => 'YellowMobile',
+   'Description' => "A Bright Yellow design for Mobile Browsers",
+   'Version' => '1.1',
+   'Author' => "VrijVlinder",
+   'AuthorEmail' => 'contact@vrijvlinder.com',
+   'AuthorUrl' => 'http://vrijvlinder.com'
+);
